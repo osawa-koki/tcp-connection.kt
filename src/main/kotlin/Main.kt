1dd6a5a771e5eb792d2bf1ba7b8d2e287d042cd4
osawa-koki <koki@osawa.dev>
2023-08-04 15:12:50 +0900

    TCP通信用のコードを書く。

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
-fun main(args: Array<String>) {
-  println("Hello World!")
 
-  // Try adding program arguments via Run/Debug configuration.
-  // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-  println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+  // Press Opt+Enter with your caret at the highlighted text to see how
+  // IntelliJ IDEA suggests fixing it.
+  println("")
+  println("----- Hello and welcome! -----")
+  println("")
+  try {
+    val server = Server()
+    server.start()
+    Client.run()
+    println("All completed!")
+
+    // プログラムを終了する。
+    System.exit(0)
+  } catch (e: Exception) {
+    e.printStackTrace()
+  }
 }
